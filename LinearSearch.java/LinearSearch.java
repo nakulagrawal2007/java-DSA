3bc8d83237c753962f2d5941dbddbe3c7908e83a
Nakul Agrawal <nakulagrawal2007420@gmail.com>
2026-08-15 15:55:38 +0530

    Added Linear Search

---
+++ LinearSearch.java/LinearSearch.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class LinearSearch {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int[] arr=new int[N];
+        for(int i=0;i<N;i++){
+            arr[i]=sc.nextInt();
+        }
+        int target=sc.nextInt();
+        for(int i=0;i<N;i++){
+            if(arr[i]==target){
+                System.out.println(i);
+                return;
+            }
+        }
+        System.out.println(-1);
+    }
+}
